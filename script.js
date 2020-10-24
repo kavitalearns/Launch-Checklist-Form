ac71cd99a9a3a799907fee1733b954921544df20
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-10-24 01:10:59 -0500

    changes on 10/24

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form=document.querySelector('form');
+   form.addEventListener("submit",function(event){
+     const pilot=document.querySelector('Input[name="pilotName"]');
+     
+     console.log(pilot.value);
+     const coPilot=document.querySelector('Input[name="copilotName"]');
+     const fuelLevel=document.querySelector('Input[name="fuelLevel"]');
+     const cargoMass=document.querySelector('Input[name="cargoMass"]');
+   if(pilot.value===''|| coPilot.value==='' || fuelLevel.value==='' || cargoMass.value===''){
+      alert("All fields are required!");
+   }
+     else if(!(isNaN(pilot.value)) || !(isNaN(coPilot.value)) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+      window.alert("Make sure to enter valid information for each field");
+    }
+     event.preventDefault();
+        const pilotStatus=document.getElementById("pilotStatus");
+        const copilotStatus=document.getElementById("copilotStatus");
+        pilotStatus.innerHTML=`Pilot ${pilot.value} is ready for launch.`;
+        copilotStatus.innerHTML=`Co-pilot ${coPilot.value} is ready for launch.`;
+        let launchStatus=document.getElementById("launchStatus");
+        let faultyItems=document.getElementById("faultyItems");
+        let fuelStatus=document.getElementById("fuelStatus");
+        let cargoStatus=document.getElementById("cargoStatus");
+        console.log(launchStatus.innerHTML);
+        
+        function changeVisibility() {
+           faultyItems.style.visibility="visible";
+           launchStatus.innerHTML="Shuttle not ready for launch";
+         launchStatus.style.color="red";
+        }
+         if (fuelLevel.value<10000 && cargoMass.value>10000) {
+             changeVisibility();
+             fuelStatus.innerHTML="Not enough fuel for the journey!";
+         cargoStatus.innerHTML="Too much mass for the shuttle to take off!";
+         } else if (fuelLevel.value<10000) {
+           changeVisibility();
+         fuelStatus.innerHTML="Not enough fuel for the journey!";
+         } else if (cargoMass.value>10000) {
+         changeVisibility();
+         cargoStatus.innerHTML="Too much mass for the shuttle to take off!";
+         } else {
+           launchStatus.innerHTML="Shuttle is ready for launch";
+         launchStatus.style.color="green";
+               }
 
+         
+      });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
