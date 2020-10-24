1f399dc71fac7640d35443f573f35371fccf7d51
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-10-24 15:00:05 -0500

    created the fetch fucntion

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ window.addEventListener("load", function(){
    let form=document.querySelector('form');
    form.addEventListener("submit",function(event){
      const pilot=document.querySelector('Input[name="pilotName"]');
-     
-     console.log(pilot.value);
      const coPilot=document.querySelector('Input[name="copilotName"]');
      const fuelLevel=document.querySelector('Input[name="fuelLevel"]');
      const cargoMass=document.querySelector('Input[name="cargoMass"]');
    if(pilot.value===''|| coPilot.value==='' || fuelLevel.value==='' || cargoMass.value===''){
       alert("All fields are required!");
-   }
-     else if(!(isNaN(pilot.value)) || !(isNaN(coPilot.value)) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+   } else if(!(isNaN(pilot.value)) || !(isNaN(coPilot.value)) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
       window.alert("Make sure to enter valid information for each field");
-    }
-     event.preventDefault();
+                      }
+     
+    event.preventDefault();
+
         const pilotStatus=document.getElementById("pilotStatus");
         const copilotStatus=document.getElementById("copilotStatus");
         pilotStatus.innerHTML=`Pilot ${pilot.value} is ready for launch.`;
@@ -43,10 +42,41 @@ window.addEventListener("load", function(){
          } else {
            launchStatus.innerHTML="Shuttle is ready for launch";
          launchStatus.style.color="green";
-               }
+                 }             
+      // fetch("https://handlers.education.launchcode.org/static/planets.json").then (function(response){
+    //        response.json().then(function(json){
+    //           let missionTarget=document.getElementById("missionTarget");
+    //           missionTarget.innerHTML=
+    //           `<h2>Mission Destination</h2>
+    //           <ol>
+    //                <li>Name: ${json[0].name}</li>
+    //               <li>Diameter: ${json[0].diameter}</li>
+    //               <li>Star: ${json[0].star}</li>
+    //               <li>Distance from Earth: ${json[0].distance}</li>
+    //               <li>Number of Moons: ${json[0].moons}</li>
+    //           </ol>
+    //               <img src="${json[0].image}"></img>`;
 
-         
-      });
+    //        });
+    fetch("https://handlers.education.launchcode.org/static/planets.json").then (function(response){
+           response.json().then(function(json){
+              let missionTarget=document.getElementById("missionTarget");
+              let randomIndex=Math.floor(Math.random()*json.length);
+             
+               missionTarget.innerHTML=
+              
+              `<h2>Mission Destination</h2>
+              <ol>
+                   <li>Name: ${json[randomIndex].name}</li>
+                  <li>Diameter: ${json[randomIndex].diameter}</li>
+                  <li>Star: ${json[randomIndex].star}</li>
+                  <li>Distance from Earth: ${json[randomIndex].distance}</li>
+                  <li>Number of Moons: ${json[randomIndex].moons}</li>
+              </ol>
+                  <img src="${json[randomIndex].image}"></img>`;
+               });
+});
+});
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
